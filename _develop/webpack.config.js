2234258e4653f1dcd5b3e176ad521b75bda3b4c4
glock18 <wnaumov@gmail.com>
2024-04-18 09:19:57 +0100

    updated files with default webpack targets

--- _develop/webpack.config.js
+++ _develop/webpack.config.js
@@ -41,16 +41,16 @@ const jsRules = {
           [
             '@babel/env',
             {
-              targets: {
-                browsers: [
-                  'last 2 Chrome major versions',
-                  'last 2 Firefox major versions',
-                  'last 2 Safari major versions',
-                  'last 2 Edge major versions',
-                  'last 2 iOS major versions',
-                  'last 2 ChromeAndroid major versions',
-                ],
-              },
+              // targets: {
+              //   browsers: [
+              //     'last 2 Chrome major versions',
+              //     'last 2 Firefox major versions',
+              //     'last 2 Safari major versions',
+              //     'last 2 Edge major versions',
+              //     'last 2 iOS major versions',
+              //     'last 2 ChromeAndroid major versions',
+              //   ],
+              // },
             },
           ],
         ],
